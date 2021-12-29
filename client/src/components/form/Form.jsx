5b2af163b31ebd5eae4de53404cbe1eb6dd52f05
cibikumar <cibikumarv@gmail.com>
2021-12-29 22:20:52 +0530

    Implemented getting and updating post

--- client/src/components/form/Form.jsx
+++ client/src/components/form/Form.jsx
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from "react";
 import { TextField, Button, Typography, Paper } from "@material-ui/core";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import FileBase from "react-file-base64";
 
 import useStyles from "./styles";
-import { createPost } from "../../actions/posts";
+import { createPost, updatePost } from "../../actions/posts";
 
 const Form = ({ currentId, setCurrentId }) => {
   const [postData, setPostData] = useState({
@@ -14,37 +14,38 @@ const Form = ({ currentId, setCurrentId }) => {
     tags: "",
     selectedFile: "",
   });
-//   const post = useSelector((state) =>
-//     currentId ? state.posts.find((message) => message._id === currentId) : null
-//   );
+  const post = useSelector((state) =>
+    currentId ? state.posts.find((message) => message._id === currentId) : null
+  );
+  
   const dispatch = useDispatch();
   const classes = useStyles();
 
-//   useEffect(() => {
-//     if (post) setPostData(post);
-//   }, [post]);
+  useEffect(() => {
+    if (post) setPostData(post);
+  }, [post]);
 
   const clear = () => {
-    // setCurrentId(0);
-    // setPostData({
-    //   creator: "",
-    //   title: "",
-    //   message: "",
-    //   tags: "",
-    //   selectedFile: "",
-    // });
+    setCurrentId(0);
+    setPostData({
+      creator: "",
+      title: "",
+      message: "",
+      tags: "",
+      selectedFile: "",
+    });
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    // if (currentId === 0) {
+    if (currentId === 0) {
       dispatch(createPost(postData));
-    //   clear();
-    // } else {
-    //   dispatch(updatePost(currentId, postData));
-    //   clear();
-    // }
+      clear();
+    } else {
+      dispatch(updatePost(currentId, postData));
+      clear();
+    }
   };
 
   return (
@@ -55,9 +56,9 @@ const Form = ({ currentId, setCurrentId }) => {
         className={`${classes.root} ${classes.form}`}
         onSubmit={handleSubmit}
       >
-        {/* <Typography variant="h6">
+        <Typography variant="h6">
           {currentId ? `Editing "${post.title}"` : "Creating a Memory"}
-        </Typography> */}
+        </Typography>
         <TextField
           name="creator"
           variant="outlined"
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { getPosts, createPosts } from "../controllers/posts.js";
+import { getPosts, createPost, updatePost } from "../controllers/posts.js";
 
 const router = express.Router();
 router
     .get("/", getPosts)
-    .post("/", createPosts);
+    .post("/", createPost)
+    .patch("/:id", updatePost);
 
 export default router;
     
